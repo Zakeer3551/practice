b08ed38b41c2582940b5aef2a46f7bfdeb7441a0
zakeer3551 <zak.ican4d@gmail.com>
2026-01-28 16:39:07 +0530

    practising daws-88s

---
+++ shell-practice/01-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1= Ramesh
+PERSON2= Suresh
+
+echo "Hi $PERSON1!:: How are you doing?"
+echo "Hi $PERSON2!:: I am doing good, How are you buddy"
+echo "$PERSON2:: I am also good $PERSON2, what are you learning these days"
+echo "$PERSON2:: You know I am learning DevOps $PERSON1"
